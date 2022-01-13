a601b7b8944d55cd23c4544910baad369d2e9755
jcpcabanada <jcpcabanada@gmail.com>
2022-01-12 19:22:23 -0800

    2nd logger

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// require your server and launch it
+// require your server and launch
+const server = require('./api/server.js')
+
+const port = 9000
+
+server.listen(port, () => {
+    console.log(`--- SERVER RUNNING ON http://localhost:${port} ---`)
+});
